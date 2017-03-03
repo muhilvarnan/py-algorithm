577f50c6e1228b248967bc38739628cbff4232d9
Muhil Varnan <muhilvarnan.v@payoda.com>
2017-03-03 19:00:18 +0530

    base commit

---
+++ py_ds/__init__.py
@@ -0,0 +1,4 @@
+import _segment_tree
+
+def test():
+	print _segment_tree.segment_tree(1.0)
--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from distutils.core import setup, Extension
 import numpy.distutils.misc_util
+from setuptools import setup, find_packages
 
 setup(
+	name='py_ds',
+    version='0.0.1',
+    description='Python datastructure module',
+    url='https://github.com/muhilvarnan/python_sms_gateway',
+    author='muhilvarnan',
+    author_email='muhilvarnan.v@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
     ext_modules=[Extension("_segment_tree", ["c/py/_segment_tree.c", "c/segment_tree.c"])],
-    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
+    include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs()
 )
